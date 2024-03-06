541116405978e6d3760e12af1854dd5de21067d4
2Kelvin <rocketmancodes02@gmail.com>
2024-03-06 06:25:18 +0300

    react-router day 2

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # React Router
 
-Learning to use react-router
+### Notes
+
++ Use `<Link to='{}></Link>` instead of <a href={}></a> to allow efficient client side routing. That is, <a> requests & fetches a new document every time you click a new url; <link> uses react router allowing us to render new url data without making a new request to the server.
--- src/index.js
+++ src/index.js
@@ -2,15 +2,33 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Root from "./routes/root";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
 import Errorpage from "./error-page";
+import Contact, { loader as contactLoader } from "./routes/contact";
+import EditContact from "./routes/edit";
 
-// root route
 const router = createBrowserRouter([
+    // root '/' route
     {
         path: "/",
         element: <Root />,
-        errorElement: <Errorpage />
+        errorElement: <Errorpage />,
+        loader: rootLoader,
+        action: rootAction,
+        children: [
+            // '/contact/:contactId' route
+            {
+                path: 'contacts/:contactId',
+                element: <Contact />,
+                loader: contactLoader,
+            },
+            // edit '/contact/:contactId' route
+            {
+                path: 'contacts/:contactId/edit',
+                element: <EditContact />,
+                loader: contactLoader,
+            },
+        ],
     },
 ]);
 
@@ -19,3 +37,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <RouterProvider router={router} />
     </React.StrictMode>
 );
+
+// We at => Updating Contacts with FormData
